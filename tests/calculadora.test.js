440ec7d50d8a228a84c95d995d0b347d2820996f
Luiz Thiago de Souza <luiz.thiago.souza@gmail.com>
2025-10-22 08:48:01 -0400

    feat: add sum function and corresponding tests

---
+++ tests/calculadora.test.js
@@ -0,0 +1,7 @@
+const calculadora = require("../models/calculadora");
+
+test("sums two numbers correctly", () => {
+  expect(calculadora.sum(2, 3)).toBe(5);
+  expect(calculadora.sum(-1, 1)).toBe(0);
+  expect(calculadora.sum(0, 0)).toBe(0);
+});
