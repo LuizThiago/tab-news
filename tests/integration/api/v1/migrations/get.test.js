df1374f2c47c706a83dde20e6b7fe7d4a78e4146
Luiz Thiago de Souza <luiz.thiago.souza@gmail.com>
2025-11-11 08:38:27 -0500

    feat: refactor database connection logic and update environment configuration

---
+++ tests/integration/api/v1/migrations/get.test.js
@@ -0,0 +1,14 @@
+import database from "infra/database";
+
+beforeAll(async () => {
+  await database.query("drop schema public cascade; create schema public;");
+});
+
+test("GET to /api/v1/migrations returns should return 200", async () => {
+  const res = await fetch("http://localhost:3000/api/v1/migrations");
+  expect(res.status).toBe(200);
+
+  const responseBody = await res.json();
+  expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
+});
